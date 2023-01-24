1fc1f7b8f10389ffd9e01cd78d67dab8413620fd
AdMarom <adenamarom@gmail.com>
2023-01-24 14:45:28 -0800

    Added function to write user answers and create readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
+const inquirer = require ('inquirer');
+const fs = require('fs').promises; 
+const generatemarkdown = require('./utils/generatemarkdowns');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => { 
+   return inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username'
+    },
+
+]);
+};
+
+const generateREADME = ({username}) =>
+`# ${username}`;
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions()
+      .then((answers) => fs.writeFile('README.md', generateREADME(answers)))
+      .then(() => console.log('Successfully wrote to README.md'))
+      .catch((err) => console.error(err));
+  };
 
 // Function call to initialize app
 init();
